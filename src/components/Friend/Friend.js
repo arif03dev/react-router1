57741daba7edb3e3c1c7a710039cb483f64a5cff
Md.Arifuzzaman <ac.arifarifuzzaman1@gmail.com>
2022-12-29 13:16:48 +0600

    add an evvent with nativigate

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Friend = ({friend}) => {
-    const {name, username, id} = friend;
+const Friend = ({ friend }) => {
+    const { name, username, id } = friend;
+    const navigate = useNavigate();
+    const friendDetails = () => {
+        const path = `friend:${id}`
+        navigate(path)
+    }
     return (
         <div>
             <h1>{name}</h1>
-            <button>{username} id:{id}</button>
+            <button onClick={friendDetails}>{username} id:{id}</button>
         </div>
     );
 };
