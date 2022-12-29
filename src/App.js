57b0cef9f5ad4d9f7c88f655e8b067224ff4fcc4
Md.Arifuzzaman <ac.arifarifuzzaman1@gmail.com>
2022-12-29 15:49:06 +0600

    add some dynamic route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import FriendDetail from './components/FriendDetail/FriendDetail';
 import Friends from './components/Friends/Friends';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <nav className='nav'>
-        <Link to="/">Home</Link>
-        <Link to="/friends">Friends</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/friends' element={<Friends />} />
+        <Route path='/friend/:friendId' element={<FriendDetail/>}/>
         <Route path='/about' element={<About />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Friend = ({ friend }) => {
     const { name, username, id } = friend;
     const navigate = useNavigate();
     const friendDetails = () => {
-        const path = `friend:${id}`
-        navigate(path)
+        // const path = `friend:${id}`
+        // navigate(path)
+        navigate("/friend/" + id)
     }
     return (
         <div>
             <h1>{name}</h1>
+            <Link to={"/friend/" + id}>Show Details</Link>
             <button onClick={friendDetails}>{username} id:{id}</button>
         </div>
     );
